810c295d8bea4fc3a0802acb19f47a64f2d39792
Kenneth Myhra <kennethmyhra@gmail.com>
2017-10-30 14:02:07 +0100

    Support GET requests
    * Support GET requests by handling header X-aes128gcm-KeyId

--- Lib.Net.Http.EncryptedContentEncoding/Aes128GcmEncodingHandler.cs
+++ Lib.Net.Http.EncryptedContentEncoding/Aes128GcmEncodingHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
 using Lib.Net.Http.EncryptedContentEncoding.Internals;
 
 namespace Lib.Net.Http.EncryptedContentEncoding
@@ -12,6 +14,8 @@ namespace Lib.Net.Http.EncryptedContentEncoding
     /// </summary>
     public sealed class Aes128GcmEncodingHandler : DelegatingHandler
     {
+        private const string X_AES128GCM_KEYID = "X-aes128gcm-KeyId";
+
         #region Fields
         private readonly Func<string, byte[]> _keyLocator;
         #endregion
@@ -36,13 +40,24 @@ namespace Lib.Net.Http.EncryptedContentEncoding
         /// <returns>The task object representing the asynchronous operation.</returns>
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue(Constants.ENCRYPTED_CONTENT_ENCODING));
+            if(request.Content.Headers.ContentEncoding.Contains(Constants.ENCRYPTED_CONTENT_ENCODING))
+            {
+                request.Content = new Aes128GcmDecodedContent(request.Content, _keyLocator);
+            }
 
             HttpResponseMessage response = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
 
-            if (response.Content.Headers.ContentEncoding.Contains(Constants.ENCRYPTED_CONTENT_ENCODING))
+            if (request.Headers.AcceptEncoding.Contains(new StringWithQualityHeaderValue(Constants.ENCRYPTED_CONTENT_ENCODING)))
             {
-                response.Content = new Aes128GcmDecodedContent(response.Content, _keyLocator);
+                if (request.Headers.TryGetValues(X_AES128GCM_KEYID, out IEnumerable<string> keyIds) && keyIds.Count() == 1)
+                {
+                    string keyId = keyIds.First();
+                    response.Content = new Aes128GcmEncodedContent(response.Content, _keyLocator(keyId), keyId, 4096);
+                    if (!response.Content.Headers.ContentEncoding.Contains(Constants.ENCRYPTED_CONTENT_ENCODING))
+                    {
+                        response.Content.Headers.ContentEncoding.Add(Constants.ENCRYPTED_CONTENT_ENCODING);
+                    }
+                }
             }
 
             return response;
